00c9cae05bd69c00f9e6d66efca30a04e803a7f8
TzivyaFireman <tz0534199716@gmail.com>
2024-07-16 17:51:17 +0300

    add roles-based access control middleware

---
+++ src/middlewares/role-guard.middleware.ts
@@ -0,0 +1,29 @@
+// src/common/middleware/roles.middleware.ts
+import { Injectable, NestMiddleware, UnauthorizedException, ForbiddenException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesMiddleware implements NestMiddleware {
+    constructor(
+        private readonly reflector: Reflector,
+    ) { }
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const user = req.user;
+        if (!user) {
+            throw new UnauthorizedException('No user information available');
+        }
+
+        const roles = this.reflector.get<string[]>('roles', req.route);
+        if (!roles || roles.length === 0) {
+            return next();
+        }
+
+        if (!roles.some(role => user.role.type === role)) {
+            throw new ForbiddenException('Forbidden');
+        }
+
+        next();
+    }
+}
