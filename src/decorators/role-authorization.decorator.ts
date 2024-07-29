53a8d945c7831a09af09900e3c74d221c8fe4165
yehudit-gershi <tehila3118@gmail.com>
2024-07-29 14:10:05 +0300

    update names of decorator and metadata

--- src/decorators/role-authorization.decorator.ts
+++ src/decorators/role-authorization.decorator.ts
@@ -1,6 +1,11 @@
-import { SetMetadata } from '@nestjs/common';
-import { Role } from './role-enum'; 
+import { SetMetadata } from 'import { SetMetadata } from '@nestjs/common';
+import { Role } from './role-enum';
+export const ROLES_METADATA_KEY = 'roles'; // משתנה קבוע לשם המטאדאטה
 
-export const Auth = (...roles: Role[]) => SetMetadata('roles', roles);
+export const RolesGuard = (...roles: Role[]) => SetMetadata(ROLES_METADATA_KEY, roles);';
+import { Role } from './role-enum';
+export const ROLES_METADATA_KEY = 'roles';
+
+export const RolesGuard = (...roles: Role[]) => SetMetadata(ROLES_METADATA_KEY, roles);
 
 
