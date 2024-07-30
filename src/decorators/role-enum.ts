141e2ffd709b64ea6ff9269fb7d146f9bcf75fa5
yehudit-gershi <tehila3118@gmail.com>
2024-07-30 12:55:01 +0300

    commit only the necessary pages

--- src/decorators/role-enum.ts
+++ src/decorators/role-enum.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ed443b26fa8b9e498084bda9b9dc428dcabb4ada16fb23b79c37fb9836c0a6a
-size 97
+oid sha256:dde2d329e7d96c97cb4c8970899c757560cc61c02c69acce4a7838de81de1a13
+size 101
